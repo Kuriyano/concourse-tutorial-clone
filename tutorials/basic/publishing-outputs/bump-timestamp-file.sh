f03df4733405ea34394c9b3dbc38fa69be6eb7ef
Kirby Au <kirby.au@outlook.com>
2023-03-19 11:21:58 +0800

    Removed config with dubious settings

--- tutorials/basic/publishing-outputs/bump-timestamp-file.sh
+++ tutorials/basic/publishing-outputs/bump-timestamp-file.sh
@@ -8,8 +8,5 @@ git clone resource-tutorial updated-gist
 cd updated-gist
 date > bumpme
 
-git config --global user.email "nobody@concourse-ci.org"
-git config --global user.name "Concourse"
-
 git add .
 git commit -m "Bumped date"
